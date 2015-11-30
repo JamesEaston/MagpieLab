453e519646d749b6851900ed1a9c565ea7a12603
James Easton <james.easton@stu.jefferson.kyschools.us>
2015-11-30 16:15:44 +0000

    Added StringTest.java

---
+++ activity2/StringTest.java
@@ -0,0 +1,10 @@
+class StringTest{
+	public static void main(String[] args) {
+		String message = "Brent's hairline is almost as bad as his mom";
+
+		System.out.println(message);
+		System.out.println(message.indexOf("unt"));
+		System.out.println(message.substring(10));
+		System.out.println(message.substring(1,3));
+	}
+}
